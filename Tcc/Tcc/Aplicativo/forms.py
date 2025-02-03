37c8b0f73b147fb7c3b84bfd8fe190d264b58015
Juarez Neto <juaresnetof@gmail.com>
2025-02-03 20:59:14 -0300

    Add files via upload

---
+++ Tcc/Tcc/Aplicativo/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from Tcc.models import Usuarios, Escola
+
+class UsuarioForm(forms.ModelForm):
+    TIPO_USUARIO_CHOICES = [
+        ('professor', 'Professor'),
+        ('usuario', 'Usuário'),
+    ]
+
+    tipo_usuario = forms.ChoiceField(choices=TIPO_USUARIO_CHOICES, widget=forms.RadioSelect)
+    esc_name = forms.CharField(max_length=45, required=False, label="Nome da Escola")
+
+    class Meta:
+        model = Usuarios
+        fields = ['usu_home', 'usu_email', 'usu_senha', 'tipo_usuario', 'esc_name']
